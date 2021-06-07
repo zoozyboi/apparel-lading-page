c6902d09b2e72836c8b964326ca32c0b9cf3fb0a
zoozyboi <79757004+zoozyboi@users.noreply.github.com>
2021-06-08 00:34:54 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var angle = 0
+
+var changeBackground = function(){
+  angle = angle + 1
+  
+  document.body.style.backgroundImage = "linear-gradient("+ angle +"deg ,hsl(0, 42%, 90%), hsl(0, 100%, 98%))"
+  console.log('rotate color');
+  requestAnimationFrame(changeBackground)
+}
+
+changeBackground();
+// 
+function myFunction() {
+  var inputObj = document.getElementById("id1");
+  if(!inputObj.checkValidity()) {
+    document.getElementById("demo").innerHTML = "Please provide a valid email";
+
+      demo2 = document.getElementById("demo2");
+      demo2.style = "display:block;"
+  }
+
+  else {
+    document.getElementById("demo").innerHTML = "";
+    demo2.style = "display:none;"
+    inputObj.placeholder = "Email Address";
+    
+  }
+}
+ 
+//   if(input.validity.typeMismatch){  
+//       input.setCustomValidity("Dude '" + input.value + "' is not a valid email. Enter something nice!!");  
+//   }  
+//   else {  
+//       input.setCustomValidity("");  
+//   }                 
+// }  
